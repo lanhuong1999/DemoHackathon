c2e19f82c0ff881d5b66ebab7467d4aee2e3153e
khanhnc1 <khanhnc1>
2023-10-14 11:39:31 +0700

    adapter

--- app/src/main/java/com/vnpay/demohackathon/ui/activities/DisplayImageAdapter.kt
+++ app/src/main/java/com/vnpay/demohackathon/ui/activities/DisplayImageAdapter.kt
@@ -3,6 +3,7 @@ package com.vnpay.demohackathon.ui.activities
 import android.graphics.Bitmap
 import android.view.View
 import android.widget.ImageView
+import androidx.core.view.isVisible
 import com.vnpay.demohackathon.R
 import com.vnpay.demohackathon.data.Photo
 import com.vnpay.demohackathon.ui.adapters.BaseRclvVH
@@ -11,10 +12,12 @@ import com.vnpay.demohackathon.ui.adapters.LoadMoreAdapter
 import com.vnpay.demohackathon.utils.Utils
 import com.vnpay.demohackathon.utils.extensions.setSafeOnClickListener
 
-class DisplayImageAdapter: LoadMoreAdapter() {
+class DisplayImageAdapter : LoadMoreAdapter() {
 
     var clickedItem: ((Any?) -> Unit)? = null
     var longClickedItem: ((Any?) -> Unit)? = null
+    private val isShowCheck = false
+    private val countCheck = 0
     override fun getLayoutResByViewType(viewType: Int): Int {
         return R.layout.display_image_item
     }
@@ -23,21 +26,21 @@ class DisplayImageAdapter: LoadMoreAdapter() {
         return DisplayImageViewHolder(itemView)
     }
 
-    inner class DisplayImageViewHolder(itemView: View): BaseRclvVH<ImageSelectVHData>(itemView){
+    inner class DisplayImageViewHolder(itemView: View) : BaseRclvVH<ImageSelectVHData>(itemView) {
         private val image: ImageView = itemView.findViewById(R.id.ivImage)
         private val icCheck: ImageView = itemView.findViewById(R.id.ivCheck)
 
         init {
             itemView.setSafeOnClickListener {
                 val position = adapterPosition
-                if (position > -1){
+                if (position > -1) {
                     clickedItem?.invoke(mDataSet[position])
                 }
             }
 
             itemView.setOnLongClickListener {
                 val position = adapterPosition
-                if (position > -1){
+                if (position > -1) {
                     longClickedItem?.invoke(mDataSet[position])
                 }
                 true
@@ -47,8 +50,14 @@ class DisplayImageAdapter: LoadMoreAdapter() {
         override fun onBind(data: ImageSelectVHData) {
             val photo = Photo(data.realData ?: "", 80, 80, image)
             Utils.g().loadImage(itemView.context, photo)
-            if (data.isCheck){
+            if (isShowCheck && !data.isCheck) {
+                icCheck.isVisible = true
+                icCheck.setImageResource(R.drawable.ic_radio_button_unselected)
+            } else if (isShowCheck && data.isCheck) {
+                icCheck.isVisible = true
                 icCheck.setImageResource(R.drawable.ic_radio_button_check)
+            } else {
+                icCheck.isVisible = false
             }
         }
 

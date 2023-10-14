aeba84705db0520136bb7250292accde2a7d743e
anhnt26 <anhnt26@vnpay.vn>
2023-10-14 14:14:21 +0700

    fix

--- app/src/main/java/com/vnpay/demohackathon/utils/Loader.kt
+++ app/src/main/java/com/vnpay/demohackathon/utils/Loader.kt
@@ -15,14 +15,20 @@ class Loader(
 
     override fun run() {
         if (photo.url != null && photo.reqHeight != null && photo.reqWidth != null) {
-            if(Utils.views[photo.imageView] == photo.url) return
+            if (check(photo)) return
             val bitmap = Utils.g().getBitmap(photo.url ?: "", photo.reqWidth ?: 0, photo.reqHeight ?: 0)
             if (bitmap != null) {
                 MemoryCache.put(photo.url ?: "", bitmap)
                 (context as Activity).runOnUiThread {
+                    if (check(photo)) return@runOnUiThread
                     photo.imageView?.setImageBitmap(bitmap)
                 }
             }
         }
+    }
+
+    fun check(photo: Photo): Boolean {
+        val str = Utils.views[photo.imageView]
+        return (str == null || str != photo.url)
     }
 }
